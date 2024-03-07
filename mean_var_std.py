15a54f2062103c1a9b6e83b894dde8d7d046e4bf
Pericvs Valle <pcvs91@gmail.com>
2024-03-07 14:35:27 +0000

    modified:   mean_var_std.py commit

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,17 @@
 import numpy as np
 
-def calculate(list):
 
-
-
-
-    return calculations
+def calculate(numbers):
+    if len(numbers) < 9:
+        raise ValueError('List must contain nine numbers.')
+        sys.exit(1)
+    a=np.reshape(numbers,(3,3))
+    output={
+        'mean':[a.mean(0).tolist(), a.mean(1).tolist(), a.mean().tolist()],
+        'variance':[a.var(0).tolist(), a.var(1).tolist(), a.var().tolist()],
+        'standard deviation':[a.std(0).tolist(), a.std(1).tolist(), a.std().tolist()],
+        'max':[a.max(0).tolist(), a.max(1).tolist(), a.max().tolist()],
+        'min':[a.min(0).tolist(), a.min(1).tolist(), a.min().tolist()],
+        'sum':[a.sum(0).tolist(), a.sum(1).tolist(), a.sum().tolist()],
+}
+    return output
